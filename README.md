55e843c3f5d9baf7d8b60d8e9dcf243ee2816695
Gatu Galindo <galindo.matias.2024@gmail.com>
2026-08-20 21:25:31 -0400

    Completado

--- README.md
+++ README.md
@@ -2,10 +2,4 @@
 
 ## TO-DO
 
-Pags bloqueadas
 
-- devolver un html con error 403: Forbidden y un gatito para las paginas bloqueadas del json
-- responder cuantos ciclos de comunicacion son necesarios para mostrar una imagen
-- hacer una pasada buscando imagenes/respuestas que ya deberiamos haber contestado
-  Test 1: Utilizando su proxy con curl intente acceder a una página prohibida y verifique que este retorna error 403 junto al código HTML que usted implementó. Verifique que al ingresar a páginas que no están prohibidas, como cc4303.bachmann.cl, su código sigue funcionando como antes.
-  Test 2: Configure su proxy momentáneamente como proxy de su navegador y vea que efectivamente se muestra la imagen que usted tiene localmente. Una vez su test sea exitoso recuerde quitar la configuración del proxy de su navegador.
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import socket
 import json
 import sys
 
-IP_VM = "10.0.2.15"
+IP_VM = "192.168.1.18"
 
 # recibe un mensaje en formato HTTP
 # retorna una 3-tupla que contiene:
@@ -10,7 +10,11 @@ IP_VM = "10.0.2.15"
 # - headers: dict
 # - body: bin
 def parse_HTTP_message(message):
-    head, body = message.split(b"\r\n\r\n")
+    try:
+        head, body = message.split(b"\r\n\r\n")
+    except ValueError:
+        head = message
+        body = b""
     decoded_head = head.decode()
     print("mensaje decodificado: " + decoded_head)
     
@@ -40,9 +44,81 @@ def create_HTTP_message(data):
     message = (start_line + "\r\n" + head + "\r\n").encode() + body
     return message
 
+def receive_full_message(connection_socket, buff_size):
+ 
+    # recibimos la primera parte del mensaje
+    recv_message = connection_socket.recv(buff_size)
+    while b"\r\n\r\n" not in recv_message:
+        recv_message += connection_socket.recv(buff_size)
+    data = parse_HTTP_message(recv_message)
+    start_line = data[0]
+    headers = data[1]
+    body = data[2]
+
+    if not start_line.startswith("HTTP"):
+        return recv_message
+
+    print(headers)
+    # entramos a un while para recibir el resto y seguimos esperando información
+    # mientras el buffer no contenga secuencia de fin de mensaje
+    body_size = int(headers["Content-Length"][1:])
+    while len(body) != body_size:
+        
+        # recibimos un nuevo trozo del mensaje
+        body += connection_socket.recv(buff_size)
+
+        # lo añadimos al mensaje "completo"
+        
+
+    # finalmente retornamos el mensaje
+    return create_HTTP_message((start_line,headers,body))
+
+# recibe los
+def error403():
+    start_line = "HTTP/1.1 403 Forbidden"
+    headers = {"Content-type":"text/html"}
+    with open("Forbidden.html") as fb:
+        body = fb.read().encode()
+    data = (start_line,headers,body)
+    return create_HTTP_message(data)
+
+def imagenResponse():
+    start_line = "HTTP/1.1 200 Ok"
+    headers = {"Content-type":"image/png"}
+    with open("Gatito.png", "rb") as miau:
+        body = miau.read()
+    data = (start_line,headers,body)
+    return create_HTTP_message(data)
+
+def addUserHeader(message, user):
+    data = parse_HTTP_message(message)
+    start_line = data[0]
+    headers = data[1]
+    body = data[2]
+    headers["X-ElQuePregunta"] = user
+    data = (start_line,headers,body)
+    return create_HTTP_message(data)
+
+def sanitize(message, badWords):
+    data = parse_HTTP_message(message)
+    start_line = data[0]
+    headers = data[1]
+    body = data[2]
+
+    for key,value in badWords.items():
+        #bkey = ''.join(format(ord(char), '08b') for char in key)
+        #bvalue = ''.join(format(ord(char), '08b') for char in value)
+        body = body.decode()
+        body = body.replace(key,value)
+        body = body.encode()
+
+    data = (start_line,headers,body)
+    return create_HTTP_message(data)
+
+
 if __name__ == "__main__":
 
-    buff_size = 10240
+    buff_size = 50
     response_headers = {}
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         path = sys.argv[1]
@@ -50,38 +126,65 @@ if __name__ == "__main__":
         with open(path) as file:
             json_data = json.load(file)
             response_headers["X-ElQuePregunta"] = json_data["user"]
+            dictProhibido = {}
+            listaDePalabrotas = json_data["forbidden_words"]
+            
+            for miniDict in listaDePalabrotas:
+                for key, value in miniDict.items():
+                    dictProhibido[key] = value
 
     con_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     con_socket.bind((IP_VM, 8000))
-    con_socket.listen(1)
-    print("esperando clientes")
+    i = 2
+    j = 0
+    con_socket.listen(i)
 
-    
-    client_socket, client_address = con_socket.accept()
-    print("cliente aceptado")
-    recv_message = client_socket.recv(buff_size)
-    print("\nMensaje Codificado:\n")
-    print(recv_message)
-    print("\n")
+    close = True
+    while close:
 
-    start_line, headers, body = parse_HTTP_message(recv_message)
-    print(start_line)
-    print(headers.keys, body)
+        print("esperando cliente")
 
-    if start_line.startswith("GET"):
-        pag = start_line.split(" ")[1] # http://example.com/
-        host = pag.replace("http://", "")
-        host = host.rstrip("/")
-        print(f"host: {host}")
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.connect((host, 80))
+        client_socket, client_address = con_socket.accept()
+        print("cliente aceptado")
+        # Se conecta el usuario del proxy
+        
+        recv_message = receive_full_message(client_socket, buff_size)
+        print("\nMensaje Codificado:\n")
+        print(recv_message)
+        print("\n")
 
-    server_socket.send(recv_message)
-    recv_message = server_socket.recv(buff_size)
-    client_socket.send(recv_message)
+        start_line, headers, body = parse_HTTP_message(recv_message)
+        print(start_line)
+        print("\n")
+        print(headers.keys, body)
 
-    client_socket.close()
-    server_socket.close()
+        if start_line.startswith("GET") or start_line.startswith("CONNECT"):
+            pag = start_line.split(" ")[1] # http://example.com/
+            pag = pag.replace("http://", "")
+            host = pag.split("/")[0]
+            pag = pag.rstrip("/")
+            print(f"host: {host}")
+
+            if pag in json_data["blocked"]:
+                recv_message = error403()
+                client_socket.send(recv_message)
+                recv_message = receive_full_message(client_socket, buff_size)
+                recv_message = imagenResponse()
+                client_socket.send(recv_message)
+            else:
+                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server_socket.connect((host, 80))
+                
+                
+                recv_message = addUserHeader(recv_message, json_data["user"])
+                server_socket.send(recv_message)
+                recv_message = receive_full_message(server_socket, buff_size)
+                # aqui hay que hacer la censura
+                recv_message = sanitize(recv_message, dictProhibido)
+                server_socket.close()
+                
+                client_socket.send(recv_message)
+        client_socket.close()
     
     # message = create_HTTP_message((response_start_line, response_headers, response_body))
     # client_socket.send(message)
